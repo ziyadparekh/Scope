3b0064d97ce3f774fcb6b188173ca19d119a3786
ziyadparekh <ziyad@1stdibs.com>
2015-02-08 15:40:48 -0500

    Added semantic css import

--- src/js/entries/app.js
+++ src/js/entries/app.js
@@ -4,8 +4,11 @@
 var BaseConfig = require('configs/BaseButtonConfig');
 var BaseView = require('views/UIBaseButtonView');
 var BaseCard = require('views/UIBaseCardView');
-var baseView = new BaseView(BaseConfig);
-var baseCard = new BaseCard(BaseConfig);
-var $ = require('jquery');
-// var KeyMap = new keymap();
-$("#canvas").html(baseCard.render().el);
+
+var Loader = require('lib/loader');
+
+Loader().onReady(function () {
+	var baseView = new BaseView(BaseConfig);
+	var baseCard = new BaseCard(BaseConfig);
+	$("#canvas").html(baseCard.render().el);
+});
